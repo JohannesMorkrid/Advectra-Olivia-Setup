36c9fd2c06b978ed0da5ddcd219f306e136fafab
JohannesMorkrid <johannes.morkrid@gmail.com>
2026-03-18 17:54:36 +0100

    Updated run script to allow for taking a script path as an argument

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=<project_number>           # project account to bill 
+#SBATCH --account=NN12110K           # project account to bill 
 #SBATCH --partition=accel            # other options are small and large
 #SBATCH --gpus-per-node=1            # Number of GPUs per node (max of 32)
 #SBATCH --mem=200G
@@ -8,8 +8,10 @@
 #SBATCH --error=error-%j
 #SBATCH --time=01:00:00              # time limit
 
+script=${1:-"simulations/script.jl"}
+
 module load NRIS/GPU  
 module load OpenMPI/5.0.9-GCC-14.3.0
 module load Julia/1.12.2
  
-julia --project=. simulations/script.jl
+julia --project=. "$script"
