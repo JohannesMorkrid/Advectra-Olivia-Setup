5ec5cffb16db3a8ba9eea1f9725a995ef1afde10
JohannesMorkrid <johannes.morkrid@gmail.com>
2026-04-21 21:31:44 +0200

    Extended parameter search

--- simulations/GD-sheath-scan/SD-LS-GD-GB.jl
+++ simulations/GD-sheath-scan/SD-LS-GD-GB.jl
@@ -47,7 +47,7 @@ diagnostics = @diagnostics [
     sample_potential(; storage_limit="15 GB"),
 ]
 
-sigmas = [1e-2, 2e-2, 5e-2, 1e-1]
+sigmas = [1e-3, 2e-3, 5e-3, 1e-2, 2e-2, 5e-2, 1e-1, 2e-1, 5e-1, 1]
 
 for σ in sigmas
     # Parameters
